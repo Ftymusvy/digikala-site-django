a9c5e7aa4912c94eb516e9a61c5cf285e6e2f215
fatemeh <fatememosavinjd98@gmail.com>
2024-08-07 15:47:43 +0330

    admin migrate , media(static)

--- digikala/digikala/urls.py
+++ digikala/digikala/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path , include
 # from shop.views import helloworld       به جای استفاده از این (include) را فراخوانی کردیم
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('' , include('shop.urls')),
-]
+]+ static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
