cca21571fe86816bdbdd0f912ec3c73094f8f9e7
Fatemeh Mousavinejhad <fatemehmosavinejad202@gmail.com>
2024-08-27 14:57:15 +0330

    product cart-delete in cart_summery

--- digikala/cart/cart.py
+++ digikala/cart/cart.py
@@ -45,3 +45,11 @@ class Cart:
 
         respond = self.cart
         return respond
+    
+    def delete(self, product):
+        product_id = str(product)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True    
